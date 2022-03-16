d64505889322cf4b31a59c91210dba63c66a95da
Mor Dvash <mordvash1@gmail.com>
2022-03-16 16:55:05 +0200

    Adding ses service

--- serverless/notification-service/serverless.ts
+++ serverless/notification-service/serverless.ts
@@ -13,6 +13,7 @@ const serverlessConfiguration: AWS = {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
+    region: 'eu-west-2',
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
--- serverless/notification-service/src/functions/sendEmail/handler.ts
+++ serverless/notification-service/src/functions/sendEmail/handler.ts
@@ -1,10 +1,42 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-
+import  { SESClient, SendEmailCommand }  from  "@aws-sdk/client-ses";
 import schema from './schema';
 
+
+const REGION = "eu-west-2"; //e.g. "us-east-1"
+const sesClient = new SESClient({ region: REGION });
+
 const sendEmail: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const params = {
+    Destination: {
+      ToAddresses: [
+        "mordvash1@gmail.com",
+      ],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: "Welcome to TradingMore",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Test mail",
+      },
+    },
+    Source: "mordvash1@gmail.com",
+  };
+
+  try {
+    const data = await sesClient.send(new SendEmailCommand(params));
+    console.log("Success", data);
+  } catch (err) {
+    console.log("Error", err);
+  }
+
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
